93047a31eca2ddcb4d924139533b7498a857325f
swc9803 <swc9803@gmail.com>
2024-03-16 02:52:18 +0900

    svg

---
+++ src/components/DropDownArrow.jsx
@@ -0,0 +1,15 @@
+const DropDownArrow = () => {
+  return (
+    <svg
+      className="dropdown_arrow"
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="11"
+      viewBox="0 0 18 11"
+    >
+      <path fill="#8F8F8F" d="M9 11 .34.5h17.32L9 11Z" />
+    </svg>
+  );
+};
+
+export default DropDownArrow;
