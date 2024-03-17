fce122ac0e84ccfff717124122f772381c1b8068
swc9803 <swc9803@gmail.com>
2024-03-17 21:03:45 +0900

    hover menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -24,29 +24,35 @@ const Header = () => {
               </Link>
             </li>
             <li>
-              <a onClick={() => goToRoute('')}>게임 정보</a>
+              <a>게임 정보</a>
               <DropDownArrow />
+              <div className="menu_hover">
+                <a>요원</a>
+                <a>맵</a>
+                <a>무기고</a>
+                <a>프리미어</a>
+              </div>
             </li>
             <li>
               <Link to="/media">미디어</Link>
             </li>
             <li>
-              <a onClick={() => goToRoute('')}>소식</a>
+              <a>소식</a>
               <DropDownArrow />
             </li>
             <li>
               <Link to="/leaderboards">순위표</Link>
             </li>
             <li>
-              <a onClick={() => goToRoute('')}>고객지원</a>
+              <a>고객지원</a>
               <DropDownArrow />
             </li>
             <li>
-              <a onClick={() => goToRoute('')}>이벤트</a>
+              <a>이벤트</a>
               <DropDownArrow />
             </li>
             <li>
-              <a onClick={() => goToRoute('')}>소셜 미디어</a>
+              <a>소셜 미디어</a>
               <DropDownArrow />
             </li>
             <li>
